f2f6ed8ffeb8952eebbdcab593df6a18cdc2f4d7
Unikwalk <vo.jennifer.n@gmail.com>
2019-05-22 20:36:19 -0700

    Removed node-modules.

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df1e2832a91f4573c48b83b366560c83.js"
+  "/precache-manifest.725788d4b21f61dc41730ef9002daf0e.js"
 );
 
 workbox.clientsClaim();
